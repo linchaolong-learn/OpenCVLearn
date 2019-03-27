ea4986bce5a1fa00fd30d09f2725a1df68f52019
lincl <lincl@ld.com>
2019-03-27 19:17:02 +0800

    图片缩放

--- 图片缩放.py
+++ 图片缩放.py
@@ -1,16 +1,34 @@
 import cv2
 
+
+def resizeImg(img):
+    """
+    缩放图片
+    """
+    # 获取图片的宽高
+    height, width, channels = img.shape
+
+    # resized = cv2.resize(img, (int(width/2), int(height/2)))
+    resized = cv2.resize(img, (int(width / 2), int(height / 2)), fx=.5, fy=.5)
+
+    return resized
+
+
+def resizeImgByAffine(img):
+    """
+    通过仿射变换缩放图片
+    """
+    return img
+
+
 img = cv2.imread('test.jpg')
 
 print(img.shape)
 
-# 获取图片的宽高
-height, width, channels = img.shape
+# resizedImg = resizeImg(img)
+resizedImg = resizeImgByAffine(img)
 
-# resized = cv2.resize(img, (int(width/2), int(height/2)))
-resized = cv2.resize(img, (int(width/2), int(height/2)), fx=.5, fy=.5)
-
-cv2.imshow('resized', resized)
+cv2.imshow('resized', resizedImg)
 
 cv2.waitKey(0)
 
@@ -18,4 +36,3 @@ cv2.waitKey(0)
 # 双线性插值
 # 像素关系重采样
 # 立方插值
-
